bd552346be0059e0a1440cb968e75823af81ac2a
Ninabaldo <nbaldorousseau@gmail.com>
2021-08-20 17:11:51 +0200

    Punt partida projecte game

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext("2d");
+const car = new Image();
+car.src = "./Imagenes/car.png"
+
+car.onload = function() {
+   ctx.drawImage(car,canvas.width/2-25,canvas.height-100,50,100)
+}
+
+}
+
+
+
+
